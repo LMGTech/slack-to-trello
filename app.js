198b52aae73c5e7038e7c823f69914761a040e41
TJ Krusinski <tjkrus@gmail.com>
2016-02-01 08:31:26 -0500

    adds comments and error checking
    TODO, add more

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var Trello = require('node-trello');
@@ -8,6 +10,10 @@ var port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+/**
+ *  Send the card to trello
+ */
+
 function postToTrello(listId, command, text, user_name, cb) {
   if (text == undefined || text == null || text == "") {
     throw new Error('Format is ' + command + ' name | description(optional)');
@@ -15,19 +21,21 @@ function postToTrello(listId, command, text, user_name, cb) {
 
   var name_and_desc = text.split('|');
 
+  if (!name_and_desc.length) throw new Error('Format is ' + command + ' name | description(optional)');
+
 	var card_data = {
 		'name' : name_and_desc.shift() + ' (@' + user_name + ')',
 		'desc' : name_and_desc.shift()
 	};
 
 	trello.post('/1/lists/' + listId + '/cards', card_data, cb);
-}
+};
 
 app.post('/*', function(req, res, next) {
   var listId = req.params[0];
-  var command = req.body.command,
-  text = req.body.text,
-  user_name = req.body.user_name;
+  var command = req.body.command;
+  var text = req.body.text;
+  var user_name = req.body.user_name;
 
   postToTrello(listId, command, text, user_name, function(err, data) {
     if (err) throw err;
@@ -41,10 +49,11 @@ app.post('/*', function(req, res, next) {
 });
 
 // test route
-app.get('/', function (req, res) { res.status(200).send('SupportKit.io loves Slack and Trello!') });
+app.get('/', function (req, res) { res.status(200).send('The server is up and running!') });
 
 // error handler
 app.use(function (err, req, res, next) {
+  err = err || new Error('There was an application error');
   console.error(err.stack);
   res.status(400).send('Error: ' + err.message);
 });
